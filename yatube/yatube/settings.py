0520f626923c5bfd2d821286a5f90f1d730e72d0
krivse <valones75@gmail.com>
2022-08-21 11:01:55 +0000

    install Postgress

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -29,6 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '84.252.141.235',
     'localhost',
+    'yatube.myddns.me',
     '127.0.0.1',
     '[::1]',
     'testserver',
@@ -83,17 +83,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
-}
+} 
 
 
 # Password validation
@@ -133,8 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+#    if not DEBUG:
+#        STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # какие страницы видит пользователь полсе входа в аккаунт.
 
 LOGIN_URL = 'users:login'
